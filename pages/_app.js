ed8bd3247abc2d788feabfbf90a9b8ef4e5215ee
oscstubler <153133305+oscstubler@users.noreply.github.com>
2023-12-06 22:22:05 +0100

    Add files via upload

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/global.css';
+// Importez d'autres fichiers CSS ou composants globaux si nécessaire
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
