cd8f6118b1fd277ab7d805edd264dc2d8ad6113e
miryamtal2k <miryam@tal2k.com>
2023-12-12 17:50:44 +0200

    batch to dvd

--- base/views.py
+++ base/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
 
 def home_view(request):
     return render(request, 'base/here.html')
