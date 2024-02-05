b8027b6b0ad4fc7b687c21acb51aa56701784774
miryamtal2k <miryam@tal2k.com>
2024-02-05 10:25:23 +0200

    db

--- base/admin.py
+++ base/admin.py
@@ -6,7 +6,7 @@ admin.site.register(Tag)
 
 
 class ArticleAdmin(admin.ModelAdmin):
-  list_display = ("title_fr",)
-  prepopulated_fields = {"slug": ("title_fr")}
+    list_display = ("title_fr",)
+    prepopulated_fields = {"slug": ("title_fr",)}  
   
 admin.site.register(Article, ArticleAdmin)
