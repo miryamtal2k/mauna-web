152947c37350ddd1f10492860e0b66112fc42a6e
miryamtal2k <miryam@tal2k.com>
2024-02-05 10:13:34 +0200

    db

--- base/models.py
+++ base/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 class Article(models.Model):
     title_fr = models.CharField(max_length=255)
     slug = models.SlugField(unique=True, help_text="A URL-friendly version of the title.")
-    content_fr = models.TextField()
+    content_fr = RichTextField() 
     publication_date = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
     tags = models.ManyToManyField('Tag')
