8b65dd5aa165b2bf5be40ca5765d231d57044418
miryamtal2k <miryam@tal2k.com>
2024-02-05 10:15:22 +0200

    db

--- base/models.py
+++ base/models.py
@@ -3,8 +3,12 @@ from ckeditor.fields import RichTextField
 
 class Article(models.Model):
     title_fr = models.CharField(max_length=255)
+    title_en = models.CharField(max_length=255)
+
     slug = models.SlugField(unique=True, help_text="A URL-friendly version of the title.")
     content_fr = RichTextField() 
+    content_en = RichTextField() 
+
     publication_date = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
     tags = models.ManyToManyField('Tag')
@@ -13,10 +17,11 @@ class Article(models.Model):
     # Add other fields as needed
 
     def __str__(self):
-        return self.title
+        return self.title_fr
 
 class Tag(models.Model):
     name_fr = models.CharField(max_length=255)
+    name_en = models.CharField(max_length=255)
 
 
     def __str__(self):
@@ -24,6 +29,7 @@ class Tag(models.Model):
 
 class Category(models.Model):
     name_fr = models.CharField(max_length=255)
+    name_en = models.CharField(max_length=255)
 
 
     def __str__(self):
