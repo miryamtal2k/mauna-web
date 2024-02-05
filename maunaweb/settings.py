20f49f38412a2a676b28ab60e2acca8b3698b55e
miryamtal2k <miryam@tal2k.com>
2024-02-05 10:00:27 +0200

    db

--- maunaweb/settings.py
+++ maunaweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n5fx8%ks$a2p$#_g$)d!mzeit(@jmhq&ilj53hy+60%jc)_v)^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
