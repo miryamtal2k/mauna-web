322f7e453979ae586dfd2d36097350f1fd7f39fc
miryamtal2k <miryam@tal2k.com>
2024-02-05 17:43:42 +0200

    db

--- base/urls.py
+++ base/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # path('', include(router.urls)),
     path('',views.home_view, name='home'),
     path('articles',views.articles_view, name='articles'),
-    path('article',views.article_view, name='article'),
+    # path('article',views.article_view, name='article'),
     path('article/<slug:slug>/', views.fullarticle_view, name='article_detail'),
 
     path('submit_form/',views.submit_form, name="submit_form"),
